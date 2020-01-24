8b4ba050c57ba08230629991eacacb909568c40f
goodflo-99 <36148760+goodflo-99@users.noreply.github.com>
2020-01-25 01:36:05 +0200

    fix

--- TextAnalyser/ClientApp/src/app/app.module.ts
+++ TextAnalyser/ClientApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CarouselModule, WavesModule, InputsModule, ButtonsModule } from 'angular-bootstrap-md'
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -12,6 +13,7 @@ import { TextAnalyserComponent } from './text-analyser/text-analyser.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { SpeakCorrectlyComponent } from './speak-correctly/speak-correctly.component';
 import { FooterComponent } from './footer/footer.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
@@ -23,16 +25,22 @@ import { FooterComponent } from './footer/footer.component';
     GalleryComponent,
     SpeakCorrectlyComponent,
     FooterComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ButtonsModule,
+    CarouselModule.forRoot(),
+    WavesModule.forRoot(),
+    InputsModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'text-analyser', component: TextAnalyserComponent },
       { path: 'gallery', component: GalleryComponent },
       { path: 'speak-correctly', component: SpeakCorrectlyComponent },
+      { path: 'card', component: CardComponent },
     ])
   ],
   providers: [],
